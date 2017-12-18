64ca8fbfdf1467118437ad6887e4a8774ab11e89
sagimonza <sagi.monza@capriza.com>
2017-12-18 17:42:34 +0200

    publicify handler/validate request

--- service.js
+++ service.js
@@ -4,21 +4,19 @@ const logger = require('./logger');
 class BusService {
     constructor(config) {
         this._name = config.name;
-        this._handlers = config.handlers;
         this._busServices = config.busServices;
         this._logger = logger(config);
-        this._buildMethodsTable();
+        this._buildMethodsTable(config.handlers);
     }
 
     async start() {
         await this._busServices.connect();
-        await this._busServices.subscribeService('approvals', this._handleRequest.bind(this));
     }
 
-    async _handleRequest(request) {
-        this._logger.debug(`'%s' start handling request for method '%s'`, this._name, request.method);
+    async handleRequest(request) {
+        this._logger.debug(`'${this._name}' start handling request for method '${request.method}'`);
         
-        this._validateRequest(request);
+        this.validateRequest(request);
 
         const handler = this._methodsToHandler[request.method];
         if (!handler) throw new Error(`[${this._name}] unknown request handler for method '${request.method}'`);
@@ -26,11 +24,11 @@ class BusService {
         return await handler.handle(request);
     }
 
-    _validateRequest(request) {}
+    validateRequest(request) {}
 
-    _buildMethodsTable() {
+    _buildMethodsTable(handlers) {
         this._methodsToHandler = {};
-        this._handlers.forEach(handler => handler.methods.forEach(method => this._methodsToHandler[method] = handler));
+        handlers.forEach(handler => handler.methods.forEach(method => this._methodsToHandler[method] = handler));
     }
 }
 
