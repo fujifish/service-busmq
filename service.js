71297ab76693abfe4fea7c01916ea63f3d30e632
sagimonza <sagi.monza@capriza.com>
2017-12-24 15:02:49 +0200

    add bus service stop

--- service.js
+++ service.js
@@ -11,7 +11,23 @@ class BusService {
 
     async start() {
         await this._busServices.connect();
-        return await this._busServices.consume(this._name, this.handleRequest.bind(this));        
+        this._service = await this._busServices.consume(this._name, this.handleRequest.bind(this));
+        return this._service;
+    }
+
+    async stop(gracePeriod) {
+        if (!this._service || !this._service.isConnected()) return;
+
+        return new Promise((resolve, reject) => {
+            const onDisconnect = $ => {
+                this._service.removeListener('disconnect', onDisconnect);
+                onDisconnect = undefined;
+                resolve();
+            };
+
+            this._service.on('disconnect', onDisconnect);
+            this._service.disconnect(gracePeriod);
+        });
     }
 
     async handleRequest(request) {
